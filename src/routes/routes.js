208be01fe9a27579be6565f83c5bd8b487fb1e12
developerOmega <developerOmega@developerOmega.com>
2020-09-16 20:20:50 -0500

    TaskLab

---
+++ src/routes/routes.js
@@ -0,0 +1,40 @@
+const routes = [
+  {
+    path: '/',
+    component: () => import('../layouts/MainLayout.vue'),
+    children: [
+      {
+        path: '',
+        component: () => import('../pages/home/Index.vue')
+      },
+      {
+        path: 'projects/create',
+        component: () => import('../pages/projects/Create.vue')
+      },
+      {
+        path: 'settings',
+        // component: () => import('../pages/settings/Main.vue'),
+        children: [
+          {
+            path: 'password',
+            component: () => import('../pages/settings/Password.vue')
+          },
+          {
+            path: 'user',
+            component: () => import('../pages/settings/User.vue')
+          }
+        ]
+      }
+    ]
+  },
+  {
+    path: '/login',
+    component: () => import('../pages/login/Index.vue'),
+  },
+  {
+    path: '/projects/:id',
+    component: () => import('../pages/projects/Show.vue')
+  }
+]
+
+export default routes;
