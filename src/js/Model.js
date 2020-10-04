b44f88096ab49d823fbfe25b93861ae72f6b1be1
developerOmega <developerOmega@developerOmega.com>
2020-10-04 15:45:45 -0500

    add static configuration

--- src/js/Model.js
+++ src/js/Model.js
@@ -2,7 +2,8 @@ import Axios from 'axios';
 
 export default class Model {
   constructor () {
-    this.url = 'http://localhost:3000';
+    // this.url = 'http://localhost:3000';
+    this.url = 'https://task-lab-backend.herokuapp.com';
     this.axios = Axios;
     this.token = localStorage.getItem('token');
     this.user = JSON.parse(localStorage.getItem('user'));
